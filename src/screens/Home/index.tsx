1a250ff93a0267750f25813b0348b4d6563d51b1
RomuloLim <romulo.lf123@gmail.com>
2023-02-04 12:57:17 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { HeaderContainer } from './styles';
+
+export function Home() {
+    return (
+        <HeaderContainer>
+            <Text>Home</Text>
+        </HeaderContainer>
+    );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components/native";
+
+export const HeaderContainer = styled.View`
+    color: #fff;
+`
